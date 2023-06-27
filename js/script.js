bdfc5e49d1729125950e3ed4a04567a1204a40b0
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-27 18:41:04 +0200

    Axios add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+/*
 //JS RESET
 console.log('JS OK')
 
 //Vue RESET
 console.log('Vue OK', Vue);
+*/
+
+//Axios RESET
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then(res => {
+    console.log(res.data.response);
+})
