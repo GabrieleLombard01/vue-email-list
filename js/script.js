f5c404ca4d4cd9b5e148f5ddba7c18ad8a09695d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-27 19:05:21 +0200

    definitive edition

--- js/script.js
+++ js/script.js
@@ -6,14 +6,29 @@ console.log('JS OK')
 console.log('Vue OK', Vue);
 */
 
+// PRENDO la UL dal dom
 const list = document.getElementById('list');
 
-//Axios 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(res => {
+    // Funzione per generare i valori res.data.response
+    function generateEmails() {
+        const emails = [];
+
+        // CICLO per generare 10 e-mail
         for (let i = 0; i < 10; i++) {
-            const li = document.createElement('li');
-            li.innerText = res.data.response;
-            list.appendChild(li);
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(res => {
+                    emails.push(res.data.response);
+
+                    if (emails.length === 10) {
+                    // Tutti i valori sono stati generati, li aggiungo alla pagina
+                    for (let j = 0; j < emails.length; j++) {
+                        const li = document.createElement('li');
+                        li.innerText = emails[j];
+                        list.appendChild(li);
+                        }
+                    }
+                });
         }
-    });
+    }
+
+    generateEmails();
